f02497b1879061e85b879a6c6b30d7e269b7af0d
safaatchane <43858585+safaatchane@users.noreply.github.com>
2019-05-22 18:22:52 +0200

    Create board.py

---
+++ board.py
@@ -0,0 +1,14 @@
+t=[]
+t.append((6,2))
+t.append((5,2))
+t.append((5,3))
+t.append((5,4))
+t.append((5,5))
+t.append((4,5))
+t.append((3,5))
+(x, y)= t[1]
+print("x=", x, "y=", y)
+(x ,y) = t[0]
+t[0] = (7,2)
+
+print(t)
